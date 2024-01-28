974468e5b3348db39d5dddcbc8ba279e66f35df7
akerkeolzhatay <153883779+akerkeolzhatay@users.noreply.github.com>
2024-01-28 14:29:31 +0600

    Student.java

--- Asignment 2/src/Student.java
+++ Asignment 2/src/Student.java
@@ -1,28 +1,18 @@
-
-class Student extends Person{
+public class Student extends Person{
     private double gpa;
-
-
-    public Student(String name, String surname, double gpa) {
+    public Student(String name, String surname, double gpa){
         super(name, surname);
         this.gpa = gpa;
     }
-
     public double getGpa() {
         return gpa;
     }
-
     public void setGpa(double gpa) {
         this.gpa = gpa;
     }
-
     @Override
-    public String toString() {
-        return "Student: " + super.toString();
-    }
-
-    @Override
-    public double getPaymentAmount() {
-        return (gpa > 2.67) ? 36660.00 : 0.0;
+    public String toString(){
+        String s = super.toString();
+        return "Student: " + s;
     }
 }
