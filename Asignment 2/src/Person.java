7cc62c747113cecda44393da3d536ae0c7805b66
akerkeolzhatay <153883779+akerkeolzhatay@users.noreply.github.com>
2024-01-28 14:28:07 +0600

    Person.java

--- Asignment 2/src/Person.java
+++ Asignment 2/src/Person.java
@@ -1,43 +1,40 @@
-
-class Person implements Payable, Comparable<Person> {
+public class Person implements Payable{
     private static int idCounter = 1;
     private int id = 0;
     private String name;
     private String surname;
-
-    public Person(String name, String surname) {
-        this.name = name;
-        this.surname = surname;
+    public Person(String name, String surname){
+        this.name = this.name;
+        this.surname = this.surname;
         this.id = idCounter++;
     }
-
-
     public int getId(){
         return id;
     }
     public String getName(){
         return name;
     }
-
     public String getSurname(){
         return surname;
     }
     @Override
     public String toString(){
-        return getId() + " . " + getName() + " " + getSurname();
+        String s = getId() + "." + getName() + " " + getSurname();
+        return s;
     }
     public String getPosition(){
         return "Student";
     }
 
     @Override
-    public double getPaymentAmount(){
-        return 0.0;
+    public double getPaymentAmount() {
+        return 0;
     }
-
     @Override
-    public int compareTo(Person other){
-        return Double.compare(this.getPaymentAmount(),other.getPaymentAmount());
-    }
+    public int compareTo(Person other) {
+        double thisPayment = this.getPaymentAmount();
+        double otherPayment = other.getPaymentAmount();
 
+        return Double.compare(thisPayment, otherPayment);
+    }
 }
